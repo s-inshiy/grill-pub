9c4ccdcbb5e447b90b2b7e2079e682caf6ae519b
Slesarev Sergei <slesarev071290@gmail.com>
2016-10-13 17:56:57 +0300

    video, gallery, team

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-    $('.slider').slick({
-      dots: false,
-      arrows:true,
-      infinite: true,
-      // speed: 300,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    });
+  $('.slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    // speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+
+    $('.team__slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    // speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+
+  new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
+
 });
